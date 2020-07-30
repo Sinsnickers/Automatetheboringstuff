1b069af79ca50f27711ebc461d9ec4a652067ee6
Sinsnickers <bero81@gmx.de>
2020-07-30 15:28:43 +0200

    playing around

--- Chapter6/zombieDice.py
+++ Chapter6/zombieDice.py
@@ -1,14 +1,15 @@
 import zombiedice
 
-class MyZombie:
+class twoRollsandRollAtZeroBrains:
     def __init__(self, name):
         # All zombies must have a name:
         self.name = name
 
     def turn(self, gameState):
-        print("First Hand" + str(zombiedice.GAME_STATE["CURRENT_HAND"]))
+        thisZombie = gameState['CURRENT_ZOMBIE']
+        highestScoreThatIsntMine = max([zombieScore for zombieName, zombieScore in gameState['SCORES'].items() if zombieName != thisZombie])
         diceRollResults = zombiedice.roll()
-        print("Second Hand" + str(zombiedice.GAME_STATE["CURRENT_HAND"]))
+        #print(diceRollResults)
         # Example of a roll() return value:
         # {'brains': 1, 'footsteps': 1, 'shotgun': 1,
         #  'rolls': [('yellow', 'brains'), ('red', 'footsteps'),
@@ -16,29 +17,73 @@ class MyZombie:
 
         # REPLACE THIS ZOMBIE CODE WITH YOUR OWN:
         shotguns = 0
+        brains = 0
         while diceRollResults is not None:
             shotguns += diceRollResults['shotgun']
+            brains += diceRollResults["brains"]
             #print(zombiedice.GAME_STATE)
+            #if brains < 13 and highestScoreThatIsntMine >= 13:
+               #diceRollResults = zombiedice.roll()
             if shotguns  <2:
                 diceRollResults = zombiedice.roll() # roll again
-            elif (shotguns==2) and ("red" in zombiedice.GAME_STATE["CURRENT_HAND"]):
+            elif shotguns == 2 and brains == 0:
+                diceRollResults = zombiedice.roll()
+            else:
+                break
+
+class twoRollsandRollAtZeroBrainsAllIn:
+    def __init__(self, name):
+        self.name = name
+
+    def turn(self, gameState):
+        thisZombie = gameState['CURRENT_ZOMBIE']
+        highestScoreThatIsntMine = max([zombieScore for zombieName, zombieScore in gameState['SCORES'].items() if zombieName != thisZombie])
+        diceRollResults = zombiedice.roll()
+        
+        shotguns = 0
+        brains = 0
+        while diceRollResults is not None:
+            shotguns += diceRollResults['shotgun']
+            brains += diceRollResults["brains"]
+            if brains <= 12 and highestScoreThatIsntMine >= 13:
+               diceRollResults = zombiedice.roll()
+            elif shotguns  <2:
                 diceRollResults = zombiedice.roll() # roll again
+            elif shotguns == 2 and brains == 0:
+                diceRollResults = zombiedice.roll()
+            else:
+                break
+
+class importedZombie:
+    def __init__(self, name):
+        self.name = name
+
+    def turn(self, gameState):
+        brains = 0
+        diceRollResults = zombiedice.roll()
+
+        while diceRollResults is not None:
+            brains += diceRollResults["brains"]
+            if brains <=3:
+                diceRollResults = zombiedice.roll()
             else:
                 break
 
 zombies = (
-    #zombiedice.examples.RandomCoinFlipZombie(name='Random'),
-    #zombiedice.examples.MonteCarloZombie(name='Monte Carlo40', riskiness=40, numExperiments=50),
-    #zombiedice.examples.MonteCarloZombie(name='Monte Carlo50', riskiness=50, numExperiments=50),
+    zombiedice.examples.RandomCoinFlipZombie(name='Random'),
+    zombiedice.examples.MonteCarloZombie(name='Monte Carlo40', riskiness=40, numExperiments=50),
+    zombiedice.examples.MonteCarloZombie(name='Monte Carlo50', riskiness=50, numExperiments=50),
     zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 2 Shotguns', minShotguns=2),
-    #zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 1 Shotgun', minShotguns=1),
-    MyZombie(name='My Zombie Bot'),
+    zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 1 Shotgun', minShotguns=1),
+    twoRollsandRollAtZeroBrains(name='twoRollsandRollAtZeroBrains'),
+    twoRollsandRollAtZeroBrainsAllIn(name="twoRollsandRollAtZeroBrainsAllIn"),
+    importedZombie(name="importedZombie"),
     # Add any other zombie players here.
 )
 
 # Uncomment one of the following lines to run in CLI or Web GUI mode:
-zombiedice.runTournament(zombies=zombies, numGames=1000)
-#zombiedice.runWebGui(zombies=zombies, numGames=1000)
+zombiedice.runTournament(zombies=zombies, numGames=1)
+#zombiedice.runWebGui(zombies=zombies, numGames=100000)
 
 #TODO Try to implement the optimal strat
 #Current Shotguns Rule
